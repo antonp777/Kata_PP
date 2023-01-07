9bd6c9f5a8bbeb62b67399c6ca4eb0b380b8d8aa
Anton Poluektov <antonp777@gmail.com>
2023-01-07 15:20:58 +0300

    Compleate work

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+import static java.lang.Class.forName;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/katajava";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "root";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static Connection connection;
+    public static Connection getConnection() {
+
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            //System.out.println("Connection OK");
+        } catch (SQLException|ClassNotFoundException ex) {
+            ex.printStackTrace();
+            //System.out.println("Connection Error");
+        }
+        return connection;
+    }
+
 }
